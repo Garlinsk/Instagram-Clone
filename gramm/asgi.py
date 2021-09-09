4b39e72bb93a1b12f8d5cf7522c914d5baae356b
franklin <franklinwanami@gmail.com>
2021-09-09 23:44:33 +0300

    recreted app

--- gramm/asgi.py
+++ gramm/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for movies project.
+ASGI config for gramm project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movies.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gramm.settings')
 
 application = get_asgi_application()
--- gramm/wsgi.py
+++ gramm/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for movies project.
+WSGI config for gramm project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movies.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gramm.settings')
 
 application = get_wsgi_application()
