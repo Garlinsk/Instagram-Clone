ce2697869d9d853dd8b98ccb57f05a1824d6d995
franklin <franklinwanami@gmail.com>
2021-09-13 01:12:26 +0300

    models

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='profile_pics/', blank=True, default='profile_pics/default.jpg')
+    about = models.TextField(blank=True)
+
+    def save_profile(self):
+        self.save()
+
+        img = Image.open(self.photo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return f'self.user, self.about,self.photo'
+
+
+
+
+
+    
