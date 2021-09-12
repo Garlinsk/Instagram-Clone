df406346fb67cc34c29f2ddc3223aee74d38dfa1
franklin <franklinwanami@gmail.com>
2021-09-13 00:16:30 +0300

    added bootstrap

--- gramm/settings.py
+++ gramm/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
 
+    
     'instagram.apps.InstagramConfig',
     'django.contrib.admin',
     'django.contrib.auth',
